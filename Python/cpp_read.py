3333e83272ed984ae11e6a144e96be330ed84c86
Furkan Aslan HATIK <furkanhtk>
2020-05-16 17:37:05 +0300

    for döngüsü eklendi

--- Python/cpp_read.py
+++ Python/cpp_read.py
@@ -7,11 +7,19 @@ Program yazılırken satır satır okunulmasına karar verilmiştir.
 list=[]
 list_firstbracket=[]
 list_lastbracket=[]
+# function
 list_function_index=[]
 list_function_name=[]
 list_function_variable=[]
 
-file = open("sum.cpp","r")
+# for
+list_for_index=[]
+list_for_variable=[]
+# if else else if
+
+
+
+file = open("for.cpp","r")
 data = file.readlines()
 
 print(type(data))
@@ -34,7 +42,7 @@ def find_function():
             list_function_index.append(data.index(x))
     for x in list_function_index:
         text = data[x].replace("int", "").replace(" ", "").replace("\n", "")
-        print("formatlanan {}".format(text))
+        #print("formatlanan {}".format(text))
         function_name_temp = text[:text.find("(")]
         list_function_name.append(function_name_temp)
         function_name_temp = []
@@ -46,14 +54,25 @@ def find_function():
             text = text.replace(text[:text.find(",")+1], "")
         list_function_variable.append(text)
         function_name_temp.append(text)
-        print(function_name_temp)
+
+def find_for():
+    for x in data:
+        if x.find("for") != -1 and x.find("(") != -1 and x.find(")") != -1:
+            list_for_index.append(data.index(x))
+    for x in list_for_index:
+        text = data[x].replace("for", "").replace(" ", "").replace("\n", "")
+        text = text.replace("(", "").replace(")", "")
+        text=text.split(";")
+        variable = text[0][:text[0].find("=")]
+        variable_start=text[0][text[0].find("=")+1:]
+        if text[1][text[0].find(variable) + 2].isnumeric() == True:
+            variable_conditon = text[1][text[0].find(variable) + 1]
+        else:
+            variable_conditon = text[1][text[0].find(variable) + 1:text[0].find(variable) + 3]
+        change = text[2].replace(variable, "")
+        print("Variable : {0},variable_start : {1},variable_conditon : {2},change : {3} ".format(variable,variable_start,variable_conditon,change))
 
 
-
-
-        # list_function_variable.append(text[text.find("(")+1:text.find(",")])
-        # text = text.replace(text[:text.find(",")], "")
-        # print(text)
 
 
 
@@ -62,11 +81,13 @@ def find_function():
 
 find_bracket()
 find_function()
+find_for()
 # print(list_firstbracket)
 # print(list_lastbracket)
 # print(list_function_index)
-print(list_function_name)
-print(list_function_variable)
+# print(list_function_name)
+# print(list_function_variable)
+
 
 
 
